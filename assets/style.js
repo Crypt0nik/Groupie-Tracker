a636e0906bcaf4cc40789fdb0ff1b304db7a0360
Arthur <arthur.chesse@ynov.com>
2024-02-27 14:49:48 +0100

    Groupie-Tracker V1

---
+++ assets/style.js
@@ -0,0 +1,51 @@
+function mOver(id) {
+    var target = document.getElementById(id)
+    if (target.innerText.length >= 23){
+    target.classList.add("s");
+}
+
+}
+function mOut(id) {
+    document.getElementById(id).classList.remove("s");
+}
+
+function key() {
+    mapboxgl.accessToken = '';
+    const mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+    return mapboxClient
+}
+
+function map() {
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [1, 1],
+        zoom: 0
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    return map
+}
+
+function marker(location) {
+    mapboxClient.geocoding
+        .forwardGeocode({
+            query: location,
+            autocomplete: false,
+            limit: 1
+        })
+        .send()
+        .then((response) => {
+            if (
+                !response ||
+                !response.body ||
+                !response.body.features ||
+                !response.body.features.length
+            ) {
+                console.error('Invalid response:');
+                console.error(response);
+                return;
+            }
+            const feature = response.body.features[0];
+            new mapboxgl.Marker().setLngLat(feature.center).setPopup(new mapboxgl.Popup().setHTML(location)).addTo(map);
+        });
+}
